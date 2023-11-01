d9c6bd292f571960b75bd043b6c67046e7e98557
sagorranait <sagorranait@gmail.com>
2023-11-01 19:41:42 +0600

    Upload image input design is done.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,12 +3,12 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className='pt-8 text-center'>
-      <p className='text-gray-500 text-sm md:text-base'>
+      <p className='text-gray-500 text-xs'>
         By uploading your files or using our service you agree with our 
         <span className='font-semibold underline cursor-pointer pl-1'>Terms of Service</span> and 
         <span className='font-semibold underline cursor-pointer pl-1'>Privacy Policy</span>.
       </p>
-      <p className='pt-2 text-gray-500 text-sm md:text-base'>&copy; 2023 Sagor Rana. All rights reserved.</p>
+      <p className='pt-2 text-gray-500 text-sm'>&copy; 2023 Sagor Rana. All rights reserved.</p>
     </div>
   )
 }
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
 const Gallery = () => {
-  const [images, setImages] = useState([]);
+  const [gallery, setGallery] = useState([]);
   const [selected, setSelected] = useState([]);
 
   useEffect(() => {
     axios.get('data.json')
     .then((response) => {
-      setImages(response.data);
+      setGallery(response.data);
     });
   }, []);
 
-  const handleImageClick = (id) => {
+  const imageClickHandler = (id) => {
     const isSelected = selected.includes(id);
     isSelected ? setSelected(selected.filter((imageId) => imageId !== id)) : setSelected([...selected, id]);
   };
+
+  const imageHandler = (event) => {
+    const selectedFile = event.target.files[0];
+    console.log(selectedFile);
+  }
 
 
   return (
@@ -33,24 +38,43 @@ const Gallery = () => {
       </div>
       <div className='py-8 px-8'>
         <div className="grid grid-cols-5 gap-5">
-          {images.map((image, index) => (
+          {gallery.map((image, index) => (
             <div key={image.id} className={`relative ${index === 0 ? 'col-span-2 row-span-2' : 'col-span-1 row-span-1'}`} >
               <img
                 src={image.img}
                 alt={`Product-${image.id}`}
-                className="w-full h-auto cursor-pointer border rounded-xl"
-                onClick={() => handleImageClick(image.id)}
+                className="w-full h-auto cursor-pointer border border-gray-400 rounded-xl"
+                onClick={() => imageClickHandler(image.id)}
               />
               <div className="absolute top-2 left-3">
                 <input 
                   type="checkbox"
                   className="mr-1"
                   checked={selected.includes(image.id)}
-                  onChange={()=>handleImageClick(image.id)}
+                  onChange={()=> imageClickHandler(image.id)}
                 />
               </div>
             </div>
           ))}
+          <form>
+            <div className="relative group w-full h-full flex justify-center items-center">
+              <div className="absolute inset-0 w-full h-full rounded-xl border border-dashed border-gray-400 bg-opacity-80 backdrop-blur-xl"></div>
+              <input
+                className="relative z-10 opacity-0 h-full w-full cursor-pointer" 
+                type="file"
+                name='image'
+                onChange={imageHandler}
+              />
+              <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full m-auo flex items-center justify-center p-3">
+                  <div className="space-y-6 flex items-center justify-center flex-col">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                      <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 15.75l5.159-5.159a2.25 2.25 0 013.182 0l5.159 5.159m-1.5-1.5l1.409-1.409a2.25 2.25 0 013.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 001.5-1.5V6a1.5 1.5 0 00-1.5-1.5H3.75A1.5 1.5 0 002.25 6v12a1.5 1.5 0 001.5 1.5zm10.5-11.25h.008v.008h-.008V8.25zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z" />
+                    </svg>
+                    <p className="text-center">Add Images</p>
+                  </div>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
